5afbcd91f7a8d44fd9894c1bcd70e42d26c270f0
Paula Micheli <paulagomes@ciandt.com>
2026-02-25 13:17:59 -0300

    :tada: initial commit

---
+++ src/main/kotlin/com/example/cambioservice/CambioServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.cambioservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CambioServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<CambioServiceApplication>(*args)
+}
